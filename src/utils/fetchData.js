152bb406aa3ddd4b787051cee2c1a8468aad952f
kevinr2 <kevinreve096@gmail.com>
2021-07-15 12:47:27 -0500

    comentando la fetchData y challenge

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,25 +1,30 @@
-let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;//eso es como hacer incluide en php
+// modulo para hacer las peticiones
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-const fetchData =(url_api,callback)=>{
 
-    return new Promise((resolve, reject)=>{
-        const xhttp = new XMLHttpRequest();
-        xhttp.open('GET',url_api,true);
-        xhttp.onreadystatechange = (() => {
-            if(xhttp.readyState === 4){
-                if (xhttp.readyState === 200) {
-                 (xhttp.status === 200)
-                    ? resolve(JSON.parse(xhttp.responseText))
-                    : reject(new Error('Error',url_api))
-                }
-                
-            }
-    
-        });
-       xhttp.send(); 
-
-    })
-
+// funcion principal
+const fetchData = (url_api) => {
+  return new Promise((resolve, reject) => {
+    // instanciamos la conexion
+    const xhttp = new XMLHttpRequest();
+    // abrir una conexion con el metodo, la ruta y si es asincrono
+    xhttp.open('GET', url_api, true);
+    // validacion del llamado
+    xhttp.onreadystatechange = (() => {
+      // comparamos el 4 porque eso indica que se completo la peticion
+      if(xhttp.readyState === 4){
+        // verificamos que el status este en 200, 200 es que es correcto
+        xhttp.status === 200
+          // si esta en 200
+          ? resolve(JSON.parse(xhttp.responseText))
+          // si no esta en 200
+          : reject(new Error('Error ' + url_api))
+      }
+    });
+    // por ultimo enviamos la peticion
+    xhttp.send();
+  });
 }
 
+// exportamos la funcion
 module.exports = fetchData;
